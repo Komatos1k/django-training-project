79ba3fa9cf4d5d9894644408b32bead0024656c5
petrenko_d <denys.petrenk@gmail.com>
2024-07-18 14:22:31 +0300

    Edit App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,48 @@
 import react from "react"
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom"
+import Login from "./pages/Login"
+import Home from "./pages/Home"
+import Register from "./pages/Register"
+import NotFound from "./pages/NotFound"
+import ProtectedRoute from "./components/ProtectedRoute"
+
+function Logout() {
+  localStorage.clear()
+  return <Navigate to="/login" />
+}
+
+function RegisterAndLogout() {
+  localStorage.clear()
+  return <Register />
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route 
+      path="/"
+      element={
+        <ProtectedRoute>
+          <Home />
+        </ProtectedRoute>
+      }
+      />
+      <Route 
+      path="/login"
+      element={
+        <Login />
+      }/>
+      <Route 
+      path="/register"
+      element={
+        <RegisterAndLogout />
+      }/>
+      <Route path="*" element={<NotFound />}/>
+      
+
+    </Routes>
+    </BrowserRouter>
   )
 }
 
--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -5,7 +5,7 @@ import { REFRESH_TOKEN, ACCESS_TOKEN } from "../constants"
 import { useState, useEffect } from "react"
 
 function ProtectedRoute({children}) {
-    const [isAuthorized, setisAuthorized] = useState(null);
+    var [isAuthorized, setisAuthorized] = useState(null);
 
     useEffect ( () => {
         auth().catch(() => setisAuthorized(false))
